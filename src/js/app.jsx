759b36beda9865bcce5bf8845089a5dd7942247f
Jikku Jose <jikkujose@gmail.com>
2016-06-23 01:04:08 +0530

    Use Ruby along side es6

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Header from './app/header.jsx'
-import Paragraph from './app/para.jsx'
+import Paragraph from './app/para.rb'
 
 export default class App extends React.Component {
+  para() {
+    let t = Opal.Paragraph.$new().$text();
+    console.log(t);
+    return(t);
+  }
+
   render() {
     return(
       <div className='container'>
         <Header />
+        <p>{this.para()}</p>
       </div>
     );
   }
