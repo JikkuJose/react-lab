2e8a3902233baab1de3dfa8f6c1626a3e32f7f5f
Jikku Jose <jikkujose@gmail.com>
2016-06-27 16:36:03 +0530

    Remove calendar component

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -2,22 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Header from './app/header.jsx'
-import CalendarHeatmap from 'react-calendar-heatmap'
 
 export default class App extends React.Component {
   render() {
     return(
       <div className='container'>
         <Header />
-        <CalendarHeatmap
-          endDate={new Date('2016-04-01')}
-          numDays={300}
-          values={[
-            { date: '2016-01-01' },
-            { date: '2016-01-22' },
-            { date: '2016-01-30' },
-          ]}
-        />
       </div>
     );
   }
