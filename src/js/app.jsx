603ae68f752a56affe3638e87c821648bb2630a5
Jikku Jose <jikkujose@gmail.com>
2016-06-27 14:10:21 +0530

    Remove Ruby & Algolia component

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -2,22 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Header from './app/header.jsx'
-import Paragraph from './app/para.rb'
-import PlaceSearch from './app/placeSearch.jsx'
 
 export default class App extends React.Component {
-  para() {
-    let t = Opal.Paragraph.$new().$text();
-    console.log(t);
-    return(t);
-  }
-
   render() {
     return(
       <div className='container'>
         <Header />
-        <p>{this.para()}</p>
-        <PlaceSearch />
       </div>
     );
   }
