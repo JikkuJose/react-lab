f6f2ab8348dfd5cb737ceefe87e62c83b1728ed7
Jikku Jose <jikkujose@gmail.com>
2016-06-27 15:56:10 +0530

    Commit with error on missing "katex"

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Katex from 'katex'
 
 import Header from './app/header.jsx'
+import Tex from '../../khan_academy-components/js/tex.jsx'
 
 export default class App extends React.Component {
   render() {
     return(
       <div className='container'>
         <Header />
+        <div
+          dangerouslySetInnerHTML={{
+            __html: Katex.renderToString("c = \\pm\\sqrt{a^2 + b^2}")
+          }}
+            />
+        <Tex>
+          \nabla \cdot E = 4 \pi \rho
+        </Tex>
       </div>
     );
   }
