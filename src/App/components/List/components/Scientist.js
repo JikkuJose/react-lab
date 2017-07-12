1a872d68e022b5e31b7181ef555eac42744935db
Jikku Jose <jikkujose@gmail.com>
2017-07-12 20:19:15 +0530

    Fix Scientist list item

--- src/App/components/List/components/Scientist.js
+++ src/App/components/List/components/Scientist.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 export const ListItem = styled.li.attrs({
   className: 'list pa2 bg-black-30 mb2 br1 w5',
 })`
-  opacity: ${props => props.param || 1};
+  opacity: ${props => props.param === undefined ? 1 : props.param};
 `
 
 export class Scientist extends React.Component {
--- src/App/components/List/components/Scientist.stories.js
+++ src/App/components/List/components/Scientist.stories.js
@@ -4,4 +4,7 @@ import { storiesOf } from '@storybook/react';
 import { Scientist } from './Scientist'
 
 storiesOf('Scientist', module)
-  .add('Without props', () => <Scientist>Newton</Scientist>)
+  .addDecorator(story => <div style={{padding: 20}}>{story()}</div>)
+  .add('Without param', () => <Scientist>Newton</Scientist>)
+  .add('With param = 0.5', () => <Scientist param={0.5}>Newton</Scientist>)
+  .add('With param = 0', () => <Scientist param={0}>Newton</Scientist>)
