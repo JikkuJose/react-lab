e9e0831fe2c156c0c1998d441a6805344a6d2d11
Jikku Jose <jikkujose@gmail.com>
2017-07-12 23:19:59 +0530

    Disable MindMap

--- src/App/components/Map/index.stories.js
+++ src/App/components/Map/index.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { Map } from './index'
+// import { Map } from './index'
 
 storiesOf('Map', module)
-  .add('Without param', () => <Map />)
+  .add('Without param', () => <code>MindMap has issues to resolve!</code>)
